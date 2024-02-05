879b5a09db4d08ddbf7420b1f0c6d4c1a855fc43
ProgramWithGraham <gaps512@gmail.com>
2024-02-04 21:59:56 -0600

    added get/set in MasterOrder and notes in all three for setup

--- src/main/java/cookies/OrderApp.java
+++ src/main/java/cookies/OrderApp.java
@@ -1,5 +1,14 @@
 package cookies;
 
+// treat like HospitalApp with Mclean and McDream that will feed into MasterORder and CookieOrder
+
+/*
+ * Add 4 orders
+Total the boxes purchased
+Show the list
+Remove a variety and give feedback on how many boxes were removed
+Show the updated list
+ */
 public class OrderApp {
     public static void main(String[] args) {
 
